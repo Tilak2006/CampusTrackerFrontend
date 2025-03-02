c67bdbaa91b857f5d81cea89c376b2c4d408cd29
Tilak <tilakj0108@gmail.com>
2025-03-02 08:31:35 +0530

    inital

--- campus-tracker/src/App.jsx
+++ campus-tracker/src/App.jsx
@@ -1,7 +1,28 @@
-
+import { Routes, Route, useLocation } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 function App() {
+  const location = useLocation(); 
+
+  return (
+    <GoogleOAuthProvider clientId="315887135987-8bgfupft7ire7pfsa3fprqk0rtshl584.apps.googleusercontent.com">
+      <>
+        {/* this is tohide Navbar on Dashboard */}
+        {location.pathname !== "/dashboard" && <Navbar />}
+
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+
   
+        {location.pathname !== "/dashboard" && <Footer />}
+      </>
+    </GoogleOAuthProvider>
+  );
 }
 
-export default App
+export default App;
